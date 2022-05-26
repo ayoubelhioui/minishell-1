2609cc48f0d40a7f1ff226e0aeb04534ab81ebd5
Issam Jmari <issamjmari098@gmail.com>
2022-05-26 21:30:20 +0100

    fixes in unset

--- builtin_cmds/unset.c
+++ builtin_cmds/unset.c
@@ -1,5 +1,19 @@
 #include "../minishell.h"
 
+int	checkand_deletion(t_list *curr, int pos, char *temp, t_list **env)
+{
+	char	**spl;
+	char	*temp2;
+
+	spl = split_with_equ(curr->content);
+	temp2 = ft_strjoin(spl[0], "=");
+	if (!ft_strcmp(temp, temp2))
+	{
+		delete_node(env, pos);
+		return (1);
+	}
+	return (0);
+}
 void	ft_unset(t_list **env, char	**args)
 {
 	t_list	*curr;
@@ -15,11 +29,8 @@ void	ft_unset(t_list **env, char	**args)
 		pos = 0;
 		while (curr)
 		{
-			if (ft_strstr(curr->content, temp) || ft_strstr(curr->content, args[i]))
-			{
-				delete_node(env, pos);
+			if (checkand_deletion(curr, pos, temp, env))
 				break;
-			}
 			pos++;
 			curr = curr->next;
 		}
