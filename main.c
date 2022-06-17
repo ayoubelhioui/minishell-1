ca2e0cc92a1c652f4f5585489a9e3cf3623f3a57
Ayoub Elhioui <Elhioui001@gmail.com>
2022-06-17 20:48:39 +0100

    almost done

--- main.c
+++ main.c
@@ -6,26 +6,30 @@
 /*   By: ael-hiou <ael-hiou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 10:32:20 by ael-hiou          #+#    #+#             */
-/*   Updated: 2022/06/17 14:37:33 by ael-hiou         ###   ########.fr       */
+/*   Updated: 2022/06/17 19:54:21 by ael-hiou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void    fuck(t_remove_quotes_vars *var)
+void    ss()
 {
-    var->new_string[0] = 'a';
-    var->new_string[1] = 'a';
-    var->new_string[2] = 'a';
-    var->new_string[3] = '\0';
+    char *s = strdup("OK");
+    s = malloc(5);
     // printf("It Is : %s\n", var->new_string);
 
 }
+void    fuck()
+{
+    char *s = strdup("OK");
+    s = malloc(5);
+    // printf("It Is : %s\n", var->new_string);
+
+}
+
 int main()
 {
-    t_remove_quotes_vars var;
-
-    var.new_string = malloc(sizeof(char) * 4);
-    fuck(&var);
-    printf("It Is : %s\n", var.new_string);
+    fuck();
+    ss();
+    while (1);
 }
