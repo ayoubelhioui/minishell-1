b614d9060c9c9b609671d58c45666ff2cd1a96dc
Ayoub Elhioui <Elhioui001@gmail.com>
2022-06-24 18:00:52 +0100

    parsing norminette almost done

--- parsing/errors_handling/errors_handling.h
+++ parsing/errors_handling/errors_handling.h
@@ -1,21 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error_handling.h                                   :+:      :+:    :+:   */
+/*   errors_handling.h                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ael-hiou <ael-hiou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/24 10:51:48 by ael-hiou          #+#    #+#             */
-/*   Updated: 2022/06/24 11:00:21 by ael-hiou         ###   ########.fr       */
+/*   Created: 2022/06/24 16:38:51 by ael-hiou          #+#    #+#             */
+/*   Updated: 2022/06/24 16:40:09 by ael-hiou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef ERROR_HANDLING_H
-# define ERROR_HANDLING_H
-#include "../../minishell.h"
+#ifndef ERRORS_HANDLING_H
+# define ERRORS_HANDLING_H
+# include "../../minishell.h"
 
-int check_unclosed_quotes(char *context);
-int error_handling_helper(t_error_handling_vars *vars, char *context);
-int error_handling(char *context);
+int	check_unclosed_quotes(char *context);
+int	error_handling_helper(t_error_handling_vars *vars, char *context);
+int	error_handling(char *context);
 
-# endif
+#endif
